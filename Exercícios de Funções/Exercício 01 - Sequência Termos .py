047d8ac2c3bb3b7931314da8a6a4c5097055c940
Rafael Pederiva <59066321+rafaelpederiva@users.noreply.github.com>
2020-01-22 10:37:21 -0300

    Update Exercício 01 - Sequência Termos .py

--- Exercícios de Funções/Exercício 01 - Sequência Termos .py
+++ Exercícios de Funções/Exercício 01 - Sequência Termos .py
@@ -1,10 +1,16 @@
 #Exercício 01
 ''' Faça um programa para imprimir:
-
         1
         2   2
         3   3   3
         .....
          n   n   n   n   n   n  ... n
-
      para um n informado pelo usuário. Use uma função que receba um valor n inteiro e imprima até a n-ésima linha. '''
+
+def imprime(numero):
+    for i in range(1, numero + 1):
+        print('%i ' %(i) * i)
+        
+n = int(input('Informe um número: '))
+
+imprime(n)
