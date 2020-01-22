79ce836b996de3ab193a3b445a6d54fef20b8cc7
Rafael Pederiva <59066321+rafaelpederiva@users.noreply.github.com>
2020-01-22 11:55:27 -0300

    Update Exercício 03 - 3 Argumentos.py

--- Exercícios de Funções/Exercício 03 - 3 Argumentos.py
+++ Exercícios de Funções/Exercício 03 - 3 Argumentos.py
@@ -1,2 +1,8 @@
 #Exercício 03
 # Faça um programa, com uma função que necessite de três argumentos, e que forneça a soma desses três argumentos. 
+def soma(n = []):
+    for i in range(3):
+        numero = int(input('Digite o %iº número: ' %(i + 1)))
+        n.append(numero)
+    print('\nA soma dos três argumentos é: ', sum(n), '\n')
+soma()
