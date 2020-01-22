b9937656472760be5d263e317acf284d0e166c1b
Rafael Pederiva <59066321+rafaelpederiva@users.noreply.github.com>
2020-01-22 12:00:53 -0300

    Update Exercício 04 - 2 Argumentos.py

--- Exercícios de Funções/Exercício 04 - 2 Argumentos.py
+++ Exercícios de Funções/Exercício 04 - 2 Argumentos.py
@@ -1,3 +1,12 @@
 #Exercício 04
 ''' Faça um programa, com uma função que necessite de um argumento. A função retorna o valor de caractere ‘P’, se seu argumento 
 for positivo, e ‘N’, se seu argumento for zero ou negativo. '''
+def validador(numero):
+    if numero > 0:
+        print('P')
+    else:
+        print('N')
+
+n = int(input('Digite um número: '))
+print('O número é: ', end='')
+validador(n)
