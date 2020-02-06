17355d8b48c0aa1392dc3b45a4cc2a8141f8c3da
Rafael Pederiva <59066321+rafaelpederiva@users.noreply.github.com>
2020-02-06 16:58:40 -0300

    Update Exercício 13 - Desenha Moldura.py

--- Exercícios de Funções/Exercício 13 - Desenha Moldura.py
+++ Exercícios de Funções/Exercício 13 - Desenha Moldura.py
@@ -1,4 +1,28 @@
 #Exercício 13
-'''Desenha moldura. Construa uma função que desenhe um retângulo usando os caracteres ‘+’ , ‘−’ e ‘| ‘. Esta função deve receber dois 
-parâmetros, linhas e colunas, sendo que o valor por omissão é o valor mínimo igual a 1 e o valor máximo é 20. Se valores fora da 
-faixa forem informados, eles devem ser modificados para valores dentro da faixa de forma elegante. '''
+# Desenha moldura. Construa uma função que desenhe um retângulo usando os caracteres ‘+’ , ‘−’ e ‘| ‘. 
+# Esta função deve receber dois parâmetros, linhas e colunas, sendo que o valor por omissão é o valor mínimo
+# igual a 1 e o valor máximo é 20. Se valores fora da faixa forem informados, eles devem ser modificados para 
+# valores dentro da faixa de forma elegante.
+
+def desenhaMoldura(linha, coluna):
+    print('+', end='')
+    print('-' * linha, end='')
+    print('+')    
+    caracter = '|'
+    validador = 0
+    while coluna > validador:
+        print(f'{caracter}{caracter:>{linha + 1}}')
+        validador += 1
+    print('+', end='')
+    print('-' * linha, end='')
+    print('+')
+
+l = int(input('Informe o valor da linha: '))
+c = int(input('Informe o valor da coluna: '))
+
+if l > 20:
+    l = 20
+if c > 20:
+    c = 20
+
+desenhaMoldura(l, c)
