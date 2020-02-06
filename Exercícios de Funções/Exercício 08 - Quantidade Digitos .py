05d44eaceab550b94a854d159e688b9c9e46d09e
Rafael Pederiva <59066321+rafaelpederiva@users.noreply.github.com>
2020-02-06 14:14:57 -0300

    Update Exercício 08 - Quantidade Digitos .py

--- Exercícios de Funções/Exercício 08 - Quantidade Digitos .py
+++ Exercícios de Funções/Exercício 08 - Quantidade Digitos .py
@@ -1,2 +1,8 @@
 #Exercício 08
 # Faça uma função que informe a quantidade de dígitos de um determinado número inteiro informado. 
+def inteiros(numero):
+    return len((str(numero)))
+
+n = str(input('Digite um número: ')).strip()
+
+print(f'Esse número tem {inteiros(n)} dígitos')
