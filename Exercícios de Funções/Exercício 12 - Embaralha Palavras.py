d906b1056e7f0a8a4fa9e70401d555617ef27e5e
Rafael Pederiva <59066321+rafaelpederiva@users.noreply.github.com>
2020-02-06 16:07:20 -0300

    Update Exercício 12 - Embaralha Palavras.py

--- Exercícios de Funções/Exercício 12 - Embaralha Palavras.py
+++ Exercícios de Funções/Exercício 12 - Embaralha Palavras.py
@@ -1,4 +1,15 @@
 #Exercício 12
-'''Embaralha palavra. Construa uma função que receba uma string como parâmetro e devolva outra string com os carateres embaralhados. 
-Por exemplo: se função receber a palavra python, pode retornar npthyo, ophtyn ou qualquer outra combinação possível, de forma aleatória.
-Padronize em sua função que todos os caracteres serão devolvidos em caixa alta ou caixa baixa, independentemente de como foram digitados. '''
+# Embaralha palavra. Construa uma função que receba uma string como parâmetro e devolva outra string com os carateres embaralhados. 
+# Por exemplo: se função receber a palavra python, pode retornar npthyo, ophtyn ou qualquer outra combinação possível, de forma aleatória.
+# Padronize em sua função que todos os caracteres serão devolvidos em caixa alta ou caixa baixa, independentemente de como foram digitados.
+
+from random import sample
+
+def embaralhar(nome):
+    lista = sample(nome, len(nome))
+    for i in range(len(lista)):
+        print(lista[i], end='')
+
+n = input('Digite algo: ')
+
+embaralhar(n)
