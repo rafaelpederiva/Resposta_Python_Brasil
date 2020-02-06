9b0be4983e605f6c333b2148ef3396370ab8d578
Rafael Pederiva <59066321+rafaelpederiva@users.noreply.github.com>
2020-02-06 14:28:00 -0300

    Update Exercício 09 - Reverso de um Número.py

--- Exercícios de Funções/Exercício 09 - Reverso de um Número.py
+++ Exercícios de Funções/Exercício 09 - Reverso de um Número.py
@@ -1,2 +1,8 @@
 #Exercício 09
 # Reverso do número. Faça uma função que retorne o reverso de um número inteiro informado. Por exemplo: 127 -> 721. 
+def reverso(numero):
+    return str(numero[::-1])
+
+n = str(input('Digite um número: '))
+
+print(f'Número reverso: {reverso(n)}')
